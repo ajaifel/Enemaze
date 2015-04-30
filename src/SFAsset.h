6a382610bb1ffd64c39e01a1c904f345eaf7d961
Jack Baxter <jackbaxter007@btinternet.com>
2015-04-30 21:23:11 +0100

    Update SFAsset.h

--- src/SFAsset.h
+++ src/SFAsset.h
@@ -41,6 +41,7 @@ public:
   virtual void      SetNotAlive();
   virtual bool      IsAlive();
   virtual void      HandleCollision();
+  virtual void      Score();
 
   virtual bool                      CollidesWith(shared_ptr<SFAsset>);
   virtual shared_ptr<SFBoundingBox> GetBoundingBox();
