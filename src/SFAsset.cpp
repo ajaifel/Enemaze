98ebf0839352c566633327a3ab8e7eb81fe8a987
Jack Baxter <jackbaxter007@btinternet.com>
2015-04-30 11:25:21 +0100

    Update SFAsset.cpp

--- src/SFAsset.cpp
+++ src/SFAsset.cpp
@@ -18,6 +18,9 @@ SFAsset::SFAsset(SFASSETTYPE type, std::shared_ptr<SFWindow> window): type(type)
   case SFASSET_COIN:
     sprite = IMG_LoadTexture(sf_window->getRenderer(), "assets/coin.png");
     break;
+  case SFASSET_WALL:
+    sprite = IMG_LoadTexture(sf_window->getRenderer(), "assets/wall.png");
+    break;
   }
 
   if(!sprite) {
