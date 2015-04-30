9067714f13f740200586ecc0ecf6d2f985d70122
Jack Baxter <jackbaxter007@btinternet.com>
2015-04-30 21:52:59 +0100

    Update SFAsset.cpp

--- src/SFAsset.cpp
+++ src/SFAsset.cpp
@@ -22,6 +22,9 @@ SFAsset::SFAsset(SFASSETTYPE type, std::shared_ptr<SFWindow> window): type(type)
   case SFASSET_WALL:
     sprite = IMG_LoadTexture(sf_window->getRenderer(), "assets/wall.png");
     break;
+  case SFASSET_WIN:
+    sprite = IMG_LoadTexture(sf_window->getRenderer(), "assets/win.png");
+    break;
   }
 
   if(!sprite) {
